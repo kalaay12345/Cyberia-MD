ba18aafb2bba75db9afed906f36be7e17754c512
K H 4 N <khanmaxamed22@gmail.com>
2025-08-09 19:02:47 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
+  SESSION_ID: "drakonis~4nNyXahK#ED9STcu6ob98Ro081i3-bxwkbmZcYHjyvqkciPOQWy0",  // add your Session Id make sure it starts with 
   OWNER_NUMBER: "1(236)362-1958", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
